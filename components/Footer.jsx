bf928506eef1a80cf81416e8bcebd28d590dc4c8
Jules Verdadero <Julesverd@gmail.com>
2022-07-30 02:26:59 -0400

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>2022 Verd Headphones All rights reserverd</p>
+      
+    </div>
+  )
+}
+
+export default Footer
