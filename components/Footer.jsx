7ea658f01ce691a0d3fdab38091edae9f7111e2e
Julesverd <julesverd@gmail.com>
2022-08-02 23:13:32 -0400

    Footer update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 Verd Headphones All rights reserverd</p>
+      <p>2022 Verd Sound All rights reserverd</p>
       
     </div>
   )
