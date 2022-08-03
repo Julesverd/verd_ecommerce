38e05a4d285b750db0ffbe43491f6c9ed4eff136
Julesverd <julesverd@gmail.com>
2022-08-02 23:21:23 -0400

    Layout update

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>Jules Verdadero</title>
+        <title>Verd</title>
       </Head>
       <header>
         <Navbar />
